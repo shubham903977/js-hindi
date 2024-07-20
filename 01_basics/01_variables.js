05ebe1d853e85cae90a91400d7ea2c2a7461bc04
shubham903977 <156423309+shubham903977@users.noreply.github.com>
2024-07-20 11:21:29 +0000

    Data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const acountID = 1445533
+let acountEmail = "shubham@google.com"
+var acountPassword = "12345"
+acountCity = "indore"
+
+
+acountEmail = "nidhi@gmail.com"
+acountPassword = "2121211"
+acountCity = "nodia"
+
+console.log(acountID);
+console.table([acountID,acountEmail,acountPassword,acountCity])
---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,17 @@
+"used strict"; //tread all js code new version
+//alert(3 + 3)// we are using node js, not browser
+
+let Name = "shubham" // string data types
+let age = "40" //numbers 
+let LogIn = "true"
+
+// number => 2 to power of 53
+// bigint
+// string => "shubham"
+// boolean => true/false
+// null => standered lone value
+// Undefine =>
+// symbole => 
+// Object =>
+    
+console.log(typeof undefined);    
