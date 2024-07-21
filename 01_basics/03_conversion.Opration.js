b4384cc90b2ede49589b96cf0e0ab3cfacccb3da
shubham903977 <156423309+shubham903977@users.noreply.github.com>
2024-07-21 12:05:32 +0000

    conversion opration

---
+++ 01_basics/03_conversion.Opration.js
@@ -0,0 +1,30 @@
+//let score = false
+
+//console.log(typeof score);
+//console.log(typeof (score));
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false =>0
+
+//let isLoggedIN = "sh"
+//let booleanISloggedIN = Boolean(isLoggedIN)
+//console.log(booleanISloggedIN)
+
+// 1 => true , 0 =>false
+// "" => false
+// "shub" => true
+ 
+const someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+//node 01_basics/03_conversion.Opration.js
