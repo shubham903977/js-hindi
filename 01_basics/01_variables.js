bd43d195c2081a364017447d5b168511a0787c94
shubham903977 <156423309+shubham903977@users.noreply.github.com>
2024-07-22 16:58:44 +0000

    comparision

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,15 @@
+<<<<<<< HEAD
 const acountID = 1445533
 let acountEmail = "shubham@google.com"
 var acountPassword = "12345"
 acountCity = "indore"
+console.log(acountID);
+=======
+const acountID = 1445533
+let acountEmail = "shubham@google.com"
+var acountPassword = "12345"
+acountCity = "indore"
+>>>>>>> b4384cc90b2ede49589b96cf0e0ab3cfacccb3da
 
 
 acountEmail = "nidhi@gmail.com"
--- 01_basics/03_conversion.Opration.js
+++ 01_basics/03_conversion.Opration.js
@@ -18,13 +18,29 @@
 // "" => false
 // "shub" => true
  
-const someNumber = 33
+//let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+ 
+//                          OPeration
 
+let str1 = "hello"
+let str2 = "shubham"
+// let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1 + 2 + "3")
+// console.log(1 + 2 )* "3" % 4)
+//console.log(+true)
 
+let num1 , num2 , num3
+num1 = num2 = num3 = 2 + 2
 
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter)
 
 //node 01_basics/03_conversion.Opration.js
