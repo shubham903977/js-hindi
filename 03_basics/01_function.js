f0d4e4a7d7dbb76c282d726662957520b7e4e3c0
shubham903977 <156423309+shubham903977@users.noreply.github.com>
2024-11-24 11:19:10 +0000

    function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -1,37 +1,63 @@
 
 
-function sayMyname(params) {
-    console.log("s");
-    console.log("h");
-    console.log("u");
-    console.log("b");
-    console.log("h");
-    console.log("a");
-    console.log("m");
-}
-//sayMyname()
+// function sayMyname(params) {
+//     console.log("s");
+//     console.log("h");
+//     console.log("u");
+//     console.log("b");
+//     console.log("h");
+//     console.log("a");
+//     console.log("m");
+// }
+// //sayMyname()
+
+// // function addtwonumber(number1,number2) {
+// //     console.log(number1 + number2);        
+// // }
 
 // function addtwonumber(number1,number2) {
-//     console.log(number1 + number2);        
+//     //let result =number1 + number2;
+//     //console.log("shubham");
+//     return number1 + number2                
 // }
 
-function addtwonumber(number1,number2) {
-    //let result =number1 + number2;
-    //console.log("shubham");
-    return number1 + number2                
+// //const result = addtwonumber(3,5)
+
+// //console.log("Result: ",result);
+
+// function loginUsermsg(username) {
+//     if(username===undefined){
+//         console.log("Please enter username");
+//         return
+//     }
+//     return `${username} just logged in`    
+// }
+// // console.log(loginUsermsg("shubham"))
+// //function calculatecardprice(val1,val2,...num1) {
+//     //return num1   
+// //}
+// // console.log(calculatecardprice(200,400,500));
+
+const user = {
+    username: "shubham",
+    price: 199
+};
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
 }
 
-//const result = addtwonumber(3,5)
+//handleObject(user);
+//handleObject({
+   // username: "nanu",
+    //price: 300
+//})
 
-//console.log("Result: ",result);
+const Mynewarray = [200,300,400,500]
 
-function loginUsermsg(username) {
-    if(username===undefined){
-        console.log("Please enter username");
-        return
-    }
-    return `${username} just logged in`    
+function returnsecondvalue(getarray) {
+    return getarray[1]    
 }
-console.log(loginUsermsg("shubham"))
- 
+// console.log(returnsecondvalue(Mynewarray));
+console.log(returnsecondvalue([200,600,400,500]));
 
