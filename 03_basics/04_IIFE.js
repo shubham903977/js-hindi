3d3c9a2cda7bce3c891eef2ce77ba77fa3f3d76c
shubham903977 <156423309+shubham903977@users.noreply.github.com>
2024-12-29 11:12:59 +0000

    IIFE

---
+++ 03_basics/04_IIFE.js
@@ -0,0 +1,8 @@
+// Immediately Invoked Function Expression (IIFE)
+(function chai() {
+    console.log(`DB CONNECTED`);       
+})();
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+    
+}  )("shubham")
